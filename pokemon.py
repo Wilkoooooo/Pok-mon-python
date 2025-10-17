fd5ec7fd30ae99130d3af5cfb74d53042a56f696
matmatwilko <mdavyjuge@gmail.com>
2025-10-17 08:57:31 +0200

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,9 @@
 import random. 
 from math import * 
+#variables de tout le programme :
+monnaie = 0
+XP = 0
+
 #Introduction
 print ("Homme mystérieux : Bonjour jeune homme! Tu es perdu(e)? Tu ne devrais pas te promener seul(e) dans la forêt si tard le soir ! Il y a des rumeurs comme quoi cette forêt abrite les Pokemons les plus dangereux.")
 print("Homme mystérieux : Comment t'appelles tu ? ") 
@@ -17,5 +21,12 @@ if int(response_1) == 1:
 	print("Alors que vous vous approchez de la foule, l'homme au milieu de la foule vous remarque et vous interpelle ! ")
 	print("Homme mystérieux : Comment ça va depuis notre dernière rencontre ?")
 	print("{prenom_1} : Ça va mais pourquoi es-tu entouré de toute cette foule ? ")
-	print("Je viens visiter mon village natal, tu ne me connais pas ? ")
-	print("Non! Qui êtes-vous ?")
+	print("Homme mystérieux : Je viens visiter mon village natal, tu ne me connais pas ? ")
+	print("{prenom_1} : Non! Qui êtes-vous ?")
+	print("Homme mystérieux : Je suis le tout nouveau maitre Pokémon. Je viens de gagner le combat final de la ligue Pokémon")
+	print("{prenom_1} : La ligue Pokémon ? C'est quoi ? ")	
+	print("Homme mystérieux : Tu ne connais pas la ligue Pokémon ?")
+
+#Branche 2
+elif int(response_1) == 2: 
+	print("Ash : ")
