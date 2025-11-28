af9b602c02cd37a9be3f55ddfbdb27efe8950134
Wilkoooooo <baptiste.coreau90@gmail.com>
2025-11-28 15:24:29 +0100

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -55,11 +55,27 @@ def menu_combat_gobou ():
 	else: 
 		print("tu n'as pas le droit on a pas encore coder cette partie et on le fera probablement jamais")
 		
+import random 
+from math import * 
+#variables de tout le programme :
+monnaie = 50
+XP = 0
+hp_poke_joueur = 60 
+hppokefightinitial = 50 
+nbr_Flammèche = 15
+nbr_LanceFlammes = 20
+nbr_Rebondifeu = 10
+nbr_Pistolet_à_O = 15
+nbr_Siphon = 20
+nbr_Hydrocanon = 10
+nbr_pokeball = 5
+nbr_superball = 0
+nbr_hyperball = 0
 def menu_combat_poussifeu ():
-	print(f"le pokemon adverse a {hppokefightinitial} points de vie, quel voulez vous faire ? \n 1) Attaquer \n 2) Se soigner \n 3) Changer de pokémon")
+	print(f"Que voulez vous faire ? \n1) Attaquer \n2) Se soigner \n3) Changer de pokémon \n4) Pokéballs\n5) Fuir")
 	choix=int(input("choisissez le bon numéro "))
 	if choix == 1: 
-		print(f"Quelle attaque voulez vous utiliser ? \n 1)Flammèche (-15 hp) {nbr_Flammèche}/15 \n 2)Lance-Flammes (-10 hp) {nbr_Lance-Flammes}/20 \n 3)Rebondifeu (-20 hp) {nbr_Rebondifeu}/10 ")
+		print(f"Quelle attaque voulez vous utiliser ? \n 1)Flammèche (-15 hp) {nbr_Flammèche}/15 \n 2)LanceFlammes (-10 hp) {nbr_LanceFlammes}/20 \n 3)Rebondifeu (-20 hp) {nbr_Rebondifeu}/10 ")
 		quelle_attaque=int(input("choisissez le bon numéro "))
 		if quelle_attaque == 1:
 			if nbr_Flammèche > 0:
@@ -68,9 +84,9 @@ def menu_combat_poussifeu ():
 			else:
 				print("Vous n'avez plus de Flammèche, faites attention la prochaine fois ")
 		elif quelle_attaque == 2: 
-			if nbr_Lance-Flammes > 0:
+			if nbr_LanceFlammes > 0:
 				hppokefightinitial = hppokefightinitial - 10 
-				nbr_Lance-Flammes = nbr_Lance-Flammes - 1
+				nbr_LanceFlammes = nbr_LanceFlammes - 1
 			else:
 				print("Vous n'avez plus de Lance-Flammes, veuillez en chosir une autre ")
 		elif quelle_attaque == 3: 
@@ -88,16 +104,73 @@ def menu_combat_poussifeu ():
 			hp_poke_joueur = 60 
 		elif choixsoin == 3: 
 			print("Sur quelle attaque : \n1)Flammèche (-15 hp) {nbr_Flammèche}/15 \n 2)Lance-Flammes (-10 hp) {nbr_Lance-Flammes}/20 \n 3)Rebondifeu (-20 hp) {nbr_Rebondifeu}/10 ")
-			reponse_pp=int(input(f"Quel attaque choisissez vous ?")
-			if reponse_pp == 1: 
-				nbr_Flammèche = nbr_Flammèche - nbr_Flammèche + 15
+			reponse_pp=int(input(f"Quelle attaque choisissez vous ?"))
+			if reponse_pp == 1:
+			    nbr_Flammèche = nbr_Flammèche - nbr_Flammèche + 15
 			elif reponse_pp == 2: 
-				nbr_Lance-Flammes = nbr_Lance-Flammes - nbr_Lance-Flammes + 20
-			elif reponse_pp == 3: 
-				nbr_Rebondifeu = nbr_Rebondifeu - nbr_Rebondifeu + 10
-	else: 
-		print("tu n'as pas le droit on a pas encore coder cette partie et on le fera probablement jamais")
-
+			    nbr_LanceFlammes = nbr_LanceFlammes - nbr_LanceFlammes + 20
+			else:
+                nbr_Rebondifeu = nbr_Rebondifeu - nbr_Rebondifeu + 10
+	elif choix == 3:
+            fuir = False
+            print("Vous ne voullez pas combattre car vous êtes une énorme tapette et décidez de fuir le combat !")
+            fuir = True 
+	elif choix == 4: 
+            print(f"Quel Pokeball veux-tu utiliser ?\n1) Pokéball ({nbr_pokeball})\n2) Superball ({nbr_superball})\n3) Hyperball ({nbr_hyperball})")
+            choix_pokeball = int(input("Choisis le bon numéro (1,2,3)"))
+            if choix_pokeball == 1:
+                if nbr_pokeball > 0: 
+                    nbr_pokeball = nbr_pokeball - 1
+                    print("Vous lancez une Pokéball !")
+                    for i in range (3):
+                        poké_pop_compteur = 0
+                        proba_poké_pop=random.randint(1,5+i)
+	                    if proba_poké_pop == 5:
+                        	print(f"La Pokéball a pop à la secousse numéro {i+1}!")
+                        	break
+                    	else :
+                        	print(f"La Pokéball n'a pas pop à la secousse numéro {i+1}!")
+                        	poké_pop_compteur = poké_pop_compteur + 1
+                    if poké_pop_compteur % 2 != 0:
+                        print("le pokémon a été capturé. Bravo !")
+                else:
+                    print("Vous n'avez plus de Pokéball c'était pourtant écrit... veuillez faire attention la prochaine fois !")    
+            elif choix_pokeball == 2:
+                if nbr_superball > 0: 
+                    nbr_superball = nbr_superball - 1
+                    print("Vous lancez une Superball !")
+                    for i in range (3):
+                        poké_pop_compteur = 0
+                        proba_poké_pop=random.randint(1,6+i)
+                        if proba_poké_pop == 5:
+                            print(f"La Superball a pop à la secousse numéro {i+1}!")
+                            break
+                        else:
+                            print(f"La Superball n'a pas pop à la secousse numéro {i+1}!")
+                            poké_pop_compteur = poké_pop_compteur + 1
+                        if poké_pop_compteur % 2 != 0:
+                            print("le pokémon a été capturé. Bravo !")
+                else:
+                    print("Vous n'avez plus de Superball c'était pourtant écrit... veuillez faire attention la prochaine fois !")
+            elif choix_pokeball == 3:
+                if nbr_hyperball > 0: 
+                    nbr_hyperball = nbr_hyperball - 1
+                    print("Vous lancez une Hyperball !")
+                    for i in range (3):
+                        poké_pop_compteur = 0
+                        proba_poké_pop=random.randint(1,7+i)
+                        if proba_poké_pop == 5:
+                            print(f"La Hyperball a pop à la secousse numéro {i+1}!")
+                            break
+                        else:
+                            print(f"La Hyperball n'a pas pop à la secousse numéro {i+1}!")
+                            poké_pop_compteur = poké_pop_compteur + 1
+                        if poké_pop_compteur % 2 != 0:
+                            print("le pokémon a été capturé. Bravo !")
+                else:
+                    print("Vous n'avez plus de Hyperball c'était pourtant écrit... veuillez faire attention la prochaine fois !")
+            else:
+                print("tu n'as pas le droit on a pas encore coder cette partie et on le fera probablement jamais")
 def choix ():
 	print("Que voulez vous faire :")
 	print("1)Partir pour les arènes pokémons\n2)Explorer la forêt Pangorn\3)Visiter les villes à proximité\4)Explorer les environs") 
